9bcc4ad49424ec15a6b5aafb29699dd5484e0101
Zokter <alan.torres.cardona@gmail.com>
2021-05-14 17:57:12 -0700

    Update Alpha 1.02
    Nueva Update Alpha 1.02.
    Se agrego:
    *Persojane principal
    *Movimientos al personaje y salto
    *Animacion del personaje (Idle).
    *Decoracion -BASICA- al nivel 1.
    *Animacion a objeto "Grass".
    *Movimiento de camara que sigue al personaje.
    /
    Se arreglaron bug's
    *Renderizado de algunas plataformas al estar fuera de camara(Causa de algo de fallas en rendimientos minimo).
    *Fallas en algunos scripts.
    /
    Proximamente se agregara (Posibilidades):
    -Musica al nivel 1.
    -Sonidos al caminar.
    -Sistema de recoger monedas.
    -Sistema de vida.
    -Sistema de muerte al salir del escenario.
    -Menu de opciones y About us.
    
    Alpha 1.02
    Zokter
    14/05/2021

---
+++ Assets/Scripts/CH_Mov.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CH_Mov : MonoBehaviour
+{
+    //Variable global
+    public float JumpForce;
+    public float Speed;
+
+    //Declaracion de variables
+    private Rigidbody2D Rigidbody2D;
+   // private Animator Animator;
+    private float Horizontal;
+    private bool Grounded;
+    //Void para cuando inicie el Level_1
+    void Start()
+    {
+        //Tomando el componente "Rigidbody2D" de nuestro GameObjet Main_CH
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+       // Animator.GetComponent<Animator>();
+    }
+    //Void para cuando este haciendose update (Frame Rate) el Level_1
+    void Update()
+    {
+        //Saber si Horizontal es -1 O 1,, Al ser -1 Es Izquierda,, al Ser 1 Es Derecha
+        Horizontal = Input.GetAxisRaw("Horizontal");
+        //
+        if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        //Animator
+        //Animator.SetBool("runing", Horizontal != 0.0f);
+        //Debug para el Rawycast
+        Debug.DrawRay(transform.position, Vector3.down * 0.18f, Color.red);
+        //Comprobando si el personaje toca el suelo para que pueda saltar
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.18f))
+        {
+            Grounded = true;
+        }
+        else Grounded = false; 
+        //Realizar salto si se preciona Spacio
+        if (Input.GetKeyDown(KeyCode.Space) && Grounded)
+        {
+            //Se crea una funcion para poder tomar Vector2 para la fuerza.
+            Jump();
+        }
+    }
+    //Void para darle fuerza al salto. 
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up * JumpForce);
+    }
+    //Poniendo velocidad al personaje ---Solamente en X "Horizontal", la Y se queda sin movimiento Constante/Velocidad
+    private void FixedUpdate()
+    {
+        Rigidbody2D.velocity = new Vector2(Horizontal, Rigidbody2D.velocity.y);
+    }
+}
---
+++ Assets/Scripts/CamaraMov.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraMov : MonoBehaviour
+{
+    //Declaracion de variables
+    public GameObject Main_CH;
+    void Update()
+    {
+        Vector3 position = transform.position;
+        position.x = Main_CH.transform.position.x;
+        transform.position = position; 
+    }
+}
